6e9330ab90011a43d72fc9b0e6fa95268333bed9
albertovic <alberto.vicentechacon@gmail.com>
2024-03-08 10:13:47 +0100

    Calculator finished

--- 10/calculator.py
+++ 10/calculator.py
@@ -29,18 +29,38 @@ new_calculation = True
 while True:
     if new_calculation == True:
         f_number = float(input("\nWhat's the first number?: "))
+        
     for symbol in operations:
         print(symbol)
-    operator = input("Pick an operation: ")
-    s_number = float(input("\nWhat's the second number?: "))
-    calc_result = calculator(f_number, s_number, operator)
+    
+    operator_error = True
+
+    while operator_error:
+        operator = input("Pick an operation: ")
+        if operator not in operations:
+            print("\nChoose a valid operator.")
+        else:
+            s_number = float(input("\nWhat's the second number?: "))
+            calc_result = calculator(f_number, s_number, operator)
+            operator_error = False
 
     print(f"\n{f_number} {operator} {s_number} = {calc_result}")
+    
+    response_error = True
 
-    if input(f"\nType 'y' to continue calculating with {calc_result}, or type 'n' to start a new calculation: ") == 'y':
-        new_calculation = False
-        f_number = calc_result
-    else: 
-        new_calculation = True
+    while response_error == True:
+        response = input(f"\nType 'y' to continue calculating with {calc_result}, 'n' to start a new calculation, or 'x' to exit: ")
+        
+        if response == 'y':
+            new_calculation = False
+            f_number = calc_result
+            response_error = False
+        elif response == 'n': 
+            new_calculation = True
+            response_error = False
+        elif response == "x":
+            print("\nExiting application...\n")
+            exit()
+        else: print("\nChoose a valid option.")
     
     #clear()
